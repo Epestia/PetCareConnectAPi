60541b77a8938e2fb6488722ce8c7c6dfac9fe97
d.menegon <d.menegon@GOS-VDI511>
2023-12-19 14:07:02 +0100

    first commit

--- PetCareConnectDb/Table/Actions_social.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Actions_social(
-   Action_social_id VARCHAR(50),
-   Descriptions VARCHAR(50),
-   Temoignages VARCHAR(50),
-   Liens_Dons VARCHAR(50),
-   PRIMARY KEY(Action_social_id)
-);
--- PetCareConnectDb/Table/Dons.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Dons(
-   Dons_id VARCHAR(50),
-   Montants DECIMAL(15,2) NOT NULL,
-   Date_dons DATE,
-   Option_paiement VARCHAR(50),
-   Statuts VARCHAR(50),
-   PRIMARY KEY(Dons_id)
-);
